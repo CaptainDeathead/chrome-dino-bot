8c2d14ca8136435e973776be14d40b37ac5e282f
= <=>
2025-01-06 11:57:21 +0800

    AI?

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ dino_rect = pg.Rect(2575-1920, 127, 3195-2575, 339-127)
 surface = pg.Surface(dino_rect.size)
 pg.draw.rect(surface, (0, 255, 0), (0, 0, dino_rect.w, dino_rect.h), width=5)
 
-bot_playing = False
+bot_playing = 1
 
 dino = Dino(dino_rect)
 cacti_manager = CactiManager(dino, surface, bot_playing)
